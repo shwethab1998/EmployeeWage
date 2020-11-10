66137ac4f50224defd6c4e0d9d51aca5dc50282a
“Shwetha <shwethabalaji87@gmail.com>
2020-11-11 00:15:45 +0530

    PartTime/FullTime Wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,24 +1,25 @@
-import java.util.Random;
 public class EmpWage {
     public static void main(String arg[]){
-        System.out.println("WELCOME to EMPLOYEE WAGE Computation");
+        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
         int empRatePerHr=20;
+        int is_Part_Time=2;
+        int is_Full_Time=1;
         int empHrs=0;
         int empWage=0;
-        double employee=Math.random();
-        if(employee>0.5)
-        {
-            System.out.println("Employee is Present");
-            empHrs=8;
-            empWage=(empHrs*empRatePerHr);
-            System.out.println("Daily Wage is "+empWage);
+        double employee = (int)(( Math.random() * 10 ) % 3);
+        if ( employee == is_Part_Time ) {
+            System.out.println("Part Time  ");
+            empHrs = 4;
         }
-        else
-        {
-            System.out.println("Employee is Absent");
-            empHrs=0;
-            empWage=(empHrs*empRatePerHr);
-            System.out.println("Daily Wage is "+empWage);
+        else if (employee == is_Full_Time ) {
+            System.out.println("Full Time ");
+            empHrs = 8;
         }
+        else {
+            System.out.println("Employee is Absent ");
+            empHrs = 0;
+        }
+            empWage=(empHrs*empRatePerHr);
+            System.out.println("Employee Wage is "+empWage);
     }
 }
