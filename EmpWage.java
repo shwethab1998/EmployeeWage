b9e5e2d051799cb0bf8bd9553a00d4bb9dca0bbb
“Shwetha <shwethabalaji87@gmail.com>
2020-11-11 00:22:20 +0530

    Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -1,24 +1,28 @@
 import java.util.Random;
 public class EmpWage {
+	public static final int empRatePerHr=20;
+    public static final int is_Part_Time=2;
+    public static final int is_Full_Time=1;
     public static void main(String arg[]){
         System.out.println("WELCOME to EMPLOYEE WAGE Computation");
         int empRatePerHr=20;
         int empHrs=0;
         int empWage=0;
-        double employee=Math.random();
-        if(employee>0.5)
-        {
-            System.out.println("Employee is Present");
-            empHrs=8;
-            empWage=(empHrs*empRatePerHr);
-            System.out.println("Daily Wage is "+empWage);
+        int employee=(int)(( Math.random()*10)%3);
+        switch(employee){
+            case is_Part_Time:
+                System.out.println("Part Time  ");
+                empHrs = 4;
+                break;
+            case is_Full_Time:
+                System.out.println("Full Time ");
+                empHrs = 8;
+                break;
+            default:
+                System.out.println("Employee is Absent ");
+                empHrs = 0;
         }
-        else
-        {
-            System.out.println("Employee is Absent");
-            empHrs=0;
             empWage=(empHrs*empRatePerHr);
-            System.out.println("Daily Wage is "+empWage);
-        }
+            System.out.println("Employee Wage is "+empWage);
     }
 }
