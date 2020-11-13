00b66b72bc286ed7759525f8db71e95892c7d7c2
“Shwetha <shwethabalaji87@gmail.com>
2020-11-13 19:33:48 +0530

    Employee Wage of Different Companies

--- EmpWage.java
+++ EmpWage.java
@@ -1,33 +1,77 @@
-import java.util.Random;
+import java.util.*;
 public class EmpWage {
-    public static final int absent=0;
-    public static final int is_Part_Time=80;
-    public static final int is_Full_Time=16;
-
-    public void calculate_wage(){
-    int sum=0;
-    System.out.println("Day\tWage\tSum");
-    for(int d=1;d<=20;d++){
-      int employee = (int) ((Math.random() * 10) % 3);
-      switch(employee){
-      case 1:
-        sum=sum+is_Full_Time;
-        System.out.println(d+"\t"+is_Full_Time+"\t"+sum);
-        break;
-      case 2 :
-        sum=sum+is_Part_Time;
-        System.out.println(d+"\t"+is_Part_Time+"\t"+sum);
-        break;
-      case 0 :
-        sum=sum+absent;
-        System.out.println(d+"\t"+absent+"\t"+sum);
-      }
+	public static final int isPartTime=1;
+    public static final int isFullTime=2;
+    
+    static String company;
+    static int ratePerHour;
+    static int days;
+    static int maxHours;
+    EmpWage(String company,int ratePerHour,int days,int maxHours)
+    {
+        this.company=company;
+        this.ratePerHour=ratePerHour;
+        this.days=days;
+        this.maxHours=maxHours;
     }
-  }
-  
-  public static void main(String[] args) {
-    System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
-    EmpWage obj = new EmpWage();
-    obj.calculate_wage();
-  }
+    public static int getWorkingHours(int check)
+    {
+        int hours;
+        switch(check)
+        {
+            case 2:
+                hours=8;
+                break;
+            case 1:
+                hours=4;
+                break;
+            default:
+                hours=0;
+                break;
+        }
+        return hours;
+    }
+    public static int attendanceCheck()
+    {
+        int result=(int)Math.floor((Math.random()*10)%3);
+        return result;
+    }
+    public static void print(int totalDays,int workHours,int totalHours,int dailySalary,int totalSalary)
+    {
+        System.out.println(totalDays+"\t"+workHours+"\t\t"+totalHours+"\t\t"+dailySalary+"\t\t"+totalSalary);
+    }
+    public static int employeeWages()
+    {
+        int check=0,workHours=0,dailySalary=0,totalSalary=0,totalHours=0,totalDays=0;
+        System.out.println("Day\tDaily Hours\tTotal Hours\tDaily Wage\tTotal Wage");
+        while(totalHours<maxHours && totalDays<days)
+        {
+            totalDays+=1;
+            check=attendanceCheck();
+            workHours=getWorkingHours(check);
+            totalHours=totalHours+workHours;
+            dailySalary=workHours*ratePerHour;
+            totalSalary=totalHours*ratePerHour;
+            print(totalDays,workHours,totalHours,dailySalary,totalSalary);
+        }
+        return totalSalary;
+    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter no.of companies:");
+		int n=sc.nextInt();
+		for(int counter=1;counter<=n;counter++)
+		{
+			System.out.println("Enter name of company:");
+			String company=sc.next();
+			System.out.println("Enter rate per hour:");
+			int ratePerHour=sc.nextInt();
+			System.out.println("Enter number of working days:");
+			int days=sc.nextInt();
+			System.out.println("Enter maximum working hours:");
+			int maxHours=sc.nextInt();
+			EmpWage companyname=new EmpWage(company,ratePerHour,days,maxHours);
+			System.out.println("Employee wage in company "+companyname.company+" is "+companyname.employeeWages());
+		}
+	}
 }
